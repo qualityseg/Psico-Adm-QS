aeb8bd0cf848935b0c9d520626f8af98337d5064
qualityseg <140438431+MmSTRoK-br@users.noreply.github.com>
2023-07-26 08:39:41 -0300

    Update Dashboard.js

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -9,7 +9,7 @@ const Dashboard = () => {
   useEffect(() => {
     const getUserCount = async () => {
       try {
-        const response = await axios.get('https://fair-ruby-caterpillar-wig.cyclic.app/cadastro_clientes/usercount');
+        const response = await axios.get('https://fair-ruby-caterpillar-wig.cyclic.app/usercount');
         setUserCount(response.data.count);
       } catch (error) {
         console.error(error);
