af924205c56daddf5cc9c62b7f92332a0a695342
qualityseg <140438431+MmSTRoK-br@users.noreply.github.com>
2023-07-26 08:13:39 -0300

    update

--- src/layouts/Login.js
+++ src/layouts/Login.js
@@ -1,58 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from 'axios';
 import styles from './Login.module.css';
-import backgroundImage1 from './background-1.png';
-import backgroundImage2 from './background-2.png';
-import backgroundImage3 from './background-3.png';
-import icon from './icone.png'; // Importando o icone
-
-const images = [backgroundImage1, backgroundImage2, backgroundImage3];
+import icon from './icone.png'; 
 
 const Login = (props) => {
   const [usuario, setUsuario] = useState("");
   const [senha, setSenha] = useState("");
-  const [backgroundIndex, setBackgroundIndex] = useState(0);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setBackgroundIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
 
   const handleLogin = (e) => {
     e.preventDefault();
     axios.post('https://fair-ruby-caterpillar-wig.cyclic.app/login', { usuario, senha })
     .then(response => {
       console.log('Response from server:', response.data); // Log the response from the server
-  
+
       if (response.data.success) {
         localStorage.setItem('token', response.data.token);
         localStorage.setItem('username', usuario);
         localStorage.setItem('role', response.data.role);
         
         console.log('Role after login:', localStorage.getItem('role')); // Log the role after login
-  
+
         props.history.push('/admin/dashboard');
       } else {
         alert('Login falhou');
       }
     });
   };
-  
-  
-  
 
-  
   return (
-    <div className={styles.container} style={{backgroundImage: `url(${images[backgroundIndex]})`}}>
+    <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.title}>
-          <img src={icon} alt="Icon" className={styles.icon} /> {/* Adicionando o ícone */}
-          QSLib | QualitySEG Painel 
+          <div className={styles.iconContainer}>
+            <img src={icon} alt="Icon" className={styles.icon} /> {/* Adicionando o ícone */}
+          </div>
+          <div className={styles.text}>
+            LifeMed | Psicossocial  
+          </div>
         </div>
         <form onSubmit={handleLogin} className={styles.form}>
           <input type="text" value={usuario} onChange={(e) => setUsuario(e.target.value)} placeholder="Usuário" className={styles.input}/>
@@ -61,7 +45,7 @@ const Login = (props) => {
         </form>
       </div>
     </div>
-  );
+);
 };
 
 export default Login;
