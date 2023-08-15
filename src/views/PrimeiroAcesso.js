0049323d152f8ae3a82e4439ad3cb77d35c8ab8c
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-08-15 11:40:13 -0300

    styles

--- src/views/PrimeiroAcesso.js
+++ src/views/PrimeiroAcesso.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Container, Form, Button, Card, Alert, Col } from 'react-bootstrap'; // Importe os componentes necessários
 import axios from 'axios';
 
 const PrimeiroAcesso = () => {
@@ -36,31 +36,43 @@ const PrimeiroAcesso = () => {
   };
 
   return (
-    <Form>
-      {!verified ? (
-        <>
-          <Form.Group>
-            <Form.Label>Nome</Form.Label>
-            <Form.Control type="text" value={name} onChange={(e) => setName(e.target.value)} />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-          </Form.Group>
-          <Button onClick={verifyUser}>Verificar</Button>
-        </>
-      ) : (
-        <>
-          <Form.Group>
-            <Form.Label>Senha</Form.Label>
-            <Form.Control type="password" value={senha} onChange={(e) => setSenha(e.target.value)} />
-          </Form.Group>
-          <Button onClick={registerPassword}>Cadastrar senha</Button>
-        </>
-      )}
-      {error && <div className="alert alert-danger">{error}</div>}
-    </Form>
+    <div style={{ backgroundImage: 'url(https://imgur.com/AFbGNnn.png)', backgroundSize: 'cover', minHeight: '100vh' }}>
+    <Container className="mt-5" >
+      <Col md={{ span: 6, offset: 3 }} style={{ marginTop: '150px' }}>
+        <Card>
+          <Card.Body>
+            <Card.Title>{!verified ? 'Verificação de Usuário' : 'Cadastro de Senha'}</Card.Title>
+            <Form>
+              {!verified ? (
+                <>
+                  <Form.Group>
+                    <Form.Label>Nome</Form.Label>
+                    <Form.Control type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                  </Form.Group>
+                  <Button onClick={verifyUser}>Verificar</Button>
+                </>
+              ) : (
+                <>
+                  <Form.Group>
+                    <Form.Label>Senha</Form.Label>
+                    <Form.Control type="password" value={senha} onChange={(e) => setSenha(e.target.value)} />
+                  </Form.Group>
+                  <Button onClick={registerPassword}>Cadastrar senha</Button>
+                </>
+              )}
+              {error && <Alert variant="danger" className="mt-3">{error}</Alert>}
+            </Form>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Container>
+    </div>
   );
 };
+
 
 export default PrimeiroAcesso;
--- src/views/UserLogin.js
+++ src/views/UserLogin.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Container, Form, Button, Card, Alert, Col } from 'react-bootstrap';
+
 import axios from 'axios';
 import { useHistory } from 'react-router-dom'; // Importe o useHistory
-
+import './Login.css'; // Importando o arquivo CSS
 const Login = () => {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
@@ -45,20 +46,34 @@ const Login = () => {
   
   
   return (
-    <Form>
-      <Form.Group>
-        <Form.Label>Usuário</Form.Label>
-        <Form.Control type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Senha</Form.Label>
-        <Form.Control type="password" value={senha} onChange={(e) => setSenha(e.target.value)} />
-      </Form.Group>
-      {error && <div className="alert alert-danger">{error}</div>}
-      <Button onClick={handleLogin}>Logar</Button>
-      <a href="/PrimeiroAcesso">Primeiro acesso</a>
-    </Form>
+    <div style={{ backgroundImage: 'url(https://imgur.com/AFbGNnn.png)', backgroundSize: 'cover', minHeight: '100vh' }}>
+    <Container className="login-container" style={{ height: '100vh' }}>
+      <Col md={{ span: 6, offset: 3 }}>
+        <Card>
+          <Card.Header as="h5" className="text-center">Login</Card.Header>
+          <Card.Body>
+            <Form>
+              <Form.Group>
+                <Form.Label>Usuário</Form.Label>
+                <Form.Control type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Senha</Form.Label>
+                <Form.Control type="password" value={senha} onChange={(e) => setSenha(e.target.value)} />
+              </Form.Group>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Button variant="primary" onClick={handleLogin} block>Logar</Button>
+            </Form>
+            <div className="text-center mt-3">
+              <a href="/PrimeiroAcesso">Primeiro acesso</a>
+            </div>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Container>
+    </div>
   );
 };
+
 
 export default Login;
