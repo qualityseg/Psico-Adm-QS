0e81f5df7c3a94a847af9914a3e81cca0ce76076
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-08-23 11:26:34 -0300

    Update GerenciamentoInstituicao.js
    funções Excluir instituição e Editar funcionando perfeitamente. Falta apenas a função para Salvar as alterações

--- src/views/GerenciamentoInstituicao.js
+++ src/views/GerenciamentoInstituicao.js
@@ -14,6 +14,7 @@ const GerenciamentoInstituicoes = () => {
   const [editData, setEditData] = useState(null);
 
   const renderCell = (label, field, item, index, category) => {
+    const value = isEditing ? editData[category][index][field] : item[field];
     return (
       <React.Fragment>
         <tr>
@@ -22,17 +23,18 @@ const GerenciamentoInstituicoes = () => {
             {isEditing ? (
               <input
                 type="text"
-                value={editData[category][index][field]}
+                value={value || ''} // Ensure that null values are treated as an empty string
                 onChange={(e) => handleInputChange(e, index, category, field)}
               />
             ) : (
-              item[field]
+              value || '' // Also handle the case where value is null when not editing
             )}
           </td>
         </tr>
       </React.Fragment>
     );
   };
+  
   
   const handleCancel = () => {
     // Restaurar os valores originais, se necessário
@@ -44,7 +46,7 @@ const GerenciamentoInstituicoes = () => {
   
   const handleEdit = () => {
     setEditData({
-      ...detalhesInstituicao.instituicoes[0], // Acessando os detalhes da instituição selecionada
+      instituicoes: detalhesInstituicao.instituicoes ? [detalhesInstituicao.instituicoes[0]] : [],
       cargos: detalhesInstituicao.cargos ? [...detalhesInstituicao.cargos] : [],
       contatos: detalhesInstituicao.contatos ? [...detalhesInstituicao.contatos] : [],
       setores: detalhesInstituicao.setores ? [...detalhesInstituicao.setores] : [],
@@ -55,24 +57,29 @@ const GerenciamentoInstituicoes = () => {
   };
   
   
-  const handleSave = async (index) => {
-  const instituicaoToUpdate = editData; // Dados editados
-
-  try {
-    const response = await axios.put(`https://fair-ruby-caterpillar-wig.cyclic.app/instituicoes/${instituicaoToUpdate.id}`, instituicaoToUpdate);
-    if (response.status === 200) {
-      // Atualizar a lista de instituições com os novos dados
-      const updatedInstituicoes = [...instituicoes];
-      updatedInstituicoes[index] = instituicaoToUpdate;
-      setInstituicoes(updatedInstituicoes);
-      setNotification({ type: 'success', message: 'Instituição atualizada com sucesso!' });
+  const handleSave = async () => {
+    try {
+      // Example structure of the data to save
+      const dataToSave = {
+        instituicoes: [editData.instituicoes], // Ensure it's an array
+        cargos: editData.cargos,
+        contatos: editData.contatos,
+        setores: editData.setores,
+        unidades: editData.unidades,
+        usuarios: editData.usuarios,
+      };
+      console.log('Data to send:', dataToSave);
+      // Sending a POST request to the server
+      const response = await axios.post('https://fair-ruby-caterpillar-wig.cyclic.app/salvar-instituicao', dataToSave);
+  
+      // Handle the response...
+    } catch (error) {
+      // Handle error...
     }
-  } catch (error) {
-    console.error(error);
-    setNotification({ type: 'danger', message: 'Erro ao atualizar a instituição' });
-  }
-};
-
+  };
+  
+  
+  
   
 const handleDeleteInstituicao = async (id) => {
   try {
@@ -90,6 +97,18 @@ const handleDeleteInstituicao = async (id) => {
   }
 };
   
+const handleInputChange = (e, field, section) => {
+  const newEditData = { ...editData };
+  const newValue = e.target.value;
+
+  // Update the specific field within the section
+  if (newEditData[section] && newEditData[section][0]) {
+    newEditData[section][0][field] = newValue;
+  }
+
+  setEditData(newEditData);
+};
+
   useEffect(() => {
     carregarInstituicoes();
   }, []);
@@ -139,39 +158,45 @@ const handleDeleteInstituicao = async (id) => {
 const renderInstituicaoDetails = () => {
   if (!detalhesInstituicao || !detalhesInstituicao.instituicoes) return null;
 
-  const instituicaoDetails = isEditing ? editData : detalhesInstituicao.instituicoes[0];
-
-  const renderCell = (key) => (
-    isEditing ? (
-      <input
-        type="text"
-        value={instituicaoDetails[key]}
-        onChange={(e) => setEditData({ ...editData, [key]: e.target.value })}
-      />
-    ) : (
-      instituicaoDetails[key]
-    )
-  );
+  const renderCell = (field, label) => {
+    const value = isEditing ? editData.instituicoes[0][field] : detalhesInstituicao.instituicoes[0][field];
+    return (
+      <tr>
+        <td className="detail-cell">{label}</td>
+        <td className="detail-cell">
+          {isEditing ? (
+            <input
+              type="text"
+              value={value || ''} // Ensure that null values are treated as an empty string
+              onChange={(e) => handleInputChange(e, field, 'instituicoes')}
+            />
+          ) : (
+            value
+          )}
+        </td>
+      </tr>
+    );
+  };
 
   return (
-    <Table responsive bordered className="institution-details-table mb-4">
+    <Table responsive bordered className="instituicao-details-table mb-4">
       <tbody>
-        <tr><td className="detail-cell">Instituição</td><td className="detail-cell">{renderCell('instituicao')}</td></tr>
-        <tr><td className="detail-cell">CNPJ</td><td className="detail-cell">{renderCell('cnpj')}</td></tr>
-        <tr><td className="detail-cell">Inscrição Estadual</td><td className="detail-cell">{renderCell('inscricaoEstadual')}</td></tr>
-        <tr><td className="detail-cell">Razão Social</td><td className="detail-cell">{renderCell('razaoSocial')}</td></tr>
-        <tr><td className="detail-cell">Logradouro</td><td className="detail-cell">{renderCell('logradouro')}</td></tr>
-        <tr><td className="detail-cell">Número</td><td className="detail-cell">{renderCell('numero')}</td></tr>
-        <tr><td className="detail-cell">Complemento</td><td className="detail-cell">{renderCell('complemento')}</td></tr>
-        <tr><td className="detail-cell">Bairro</td><td className="detail-cell">{renderCell('bairro')}</td></tr>
-        <tr><td className="detail-cell">Cidade</td><td className="detail-cell">{renderCell('cidade')}</td></tr>
-        <tr><td className="detail-cell">Estado</td><td className="detail-cell">{renderCell('estado')}</td></tr>
-        <tr><td className="detail-cell">País</td><td className="detail-cell">{renderCell('pais')}</td></tr>
-        <tr><td className="detail-cell">CEP</td><td className="detail-cell">{renderCell('cep')}</td></tr>
+        {renderCell('instituicao', 'Instituição')}
+        {renderCell('cnpj', 'CNPJ')}
+        {renderCell('razaoSocial', 'Razão Social')}
+        {renderCell('logradouro', 'Logradouro')}
+        {renderCell('numero', 'Número')}
+        {renderCell('complemento', 'Complemento')}
+        {renderCell('bairro', 'Bairro')}
+        {renderCell('cidade', 'Cidade')}
+        {renderCell('estado', 'Estado')}
+        {renderCell('pais', 'País')}
+        {renderCell('cep', 'CEP')}
       </tbody>
     </Table>
   );
 };
+
 
 const renderCargosDetails = () => {
   if (!detalhesInstituicao || !detalhesInstituicao.cargos) return null;
